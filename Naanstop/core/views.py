27d9cc0e7ebce3dcffb808dec33414116127f4ea
1272251359-dotcom <1272251359@mitwpu.edu.in>
2025-11-13 13:11:06 +0530

    Delete Naanstop/core directory

--- Naanstop/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import ListView
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Post
-
-def home(request):
-    posts = Post.objects.all().order_by('-id')[:5]
-    return render(request, "core/home.html", {"posts": posts})
